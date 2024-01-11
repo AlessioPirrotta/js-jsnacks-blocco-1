db065f7bdc10d9bb44e392804545ce89755a0fcb
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-11 18:08:28 +0100

    exercise 1 completed

---
+++ Snack1/main.js
@@ -0,0 +1,21 @@
+// Blocco 1 - JSnack 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+let numeri = []
+let risultato= document.getElementById("rs")
+let somma= 0
+
+
+
+
+for(let i= 0; i< 10; i++){
+   let numero= prompt("scrivi un numero")
+   numeri.push(parseFloat(numero))
+   somma += numeri[i]
+}
+risultato.innerHTML= `La somma é ${somma}`
+
+
+
+
