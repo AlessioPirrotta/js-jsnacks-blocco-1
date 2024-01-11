dc0bfc4a2413040945a29b35f8c5a4a5f6929129
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-01-11 18:54:10 +0100

    second exercise completed

---
+++ Snack2/main.js
@@ -0,0 +1,19 @@
+// -Stampa il cubo dei N numeri,
+// dove N è un numero indicato dall’utente
+
+
+let numeri = []
+let risultato = document.getElementById("rs")
+let risultatoCubi = "I cubi "
+
+for(let i= 0; i< 3; i++){
+   let numero= prompt("scrivi un numero")
+   numeri.push(parseFloat(numero))
+   risultatoCubi += numeri[i] ** 3
+   if (i < 2) {
+    risultatoCubi+= ' ';
+}
+}
+
+risultato.innerHTML= risultatoCubi
+
